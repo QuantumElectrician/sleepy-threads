ad6155ad617e8e6f308732eb594604ba78ba5fdb
Vladislav <you@example.com>
2017-10-07 16:54:07 +0300

    Initial Commit

---
+++ sleepy threads/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sleepy threads
+//
+//  Created by Владислав Агафонов on 07.10.2017.
+//  Copyright © 2017 Владислав Агафонов. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
